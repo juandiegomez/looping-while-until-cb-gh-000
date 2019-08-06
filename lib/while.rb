7cb03c964ac464a36fc7523c0353d06f6aa96fbf
Juan Diego Gomez Peña <juandi.gomezp@utadeo.edu.co>
2019-08-06 14:48:45 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -2,8 +2,11 @@ def using_while
   levitation_force = 6
 	while levitation_force < 10
     puts "Almost there"
-    levitation_force +=1
+    levitation_force += 1
   end
+
   puts "Wingardium Leviosa"
   #your code here
 end
+
+
